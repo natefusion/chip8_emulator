e43cb5cb9cfdd43ba6f4765527276ab61b0f19b5
natefusion <n8thefusion@gmail.com>
2020-10-20 22:21:48 -0400

    wut

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,9 @@ fn main() {
     loop {
         my_chip8.emulate_cycle();
         if my_chip8.draw_flag {
-        my_chip8sdl.draw_frame(&my_chip8);
-    }
+            my_chip8sdl.draw_frame(&my_chip8);
+        }
         if my_chip8sdl.handle_events(&mut my_chip8) { break; }
-
-    //Remove this and instead count delay timer down at 60hz in the future
-    //let duration = duration::from_millis(16.67);
-    std::thread::sleep(Duration::from_micros(7000));
+        std::thread::sleep(Duration::from_micros(7000));
     }
 }
